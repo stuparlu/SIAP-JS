3b6c973fe61eaf7a3f8bda38a64b152ea0f6d1ac
stuparche <stupar22luka@gmail.com>
2023-03-14 12:14:32 +0100

    Looping through the projects
    + Extraction of project description
    + Started work on video and image extraction.

--- index.cjs
+++ index.cjs
@@ -1,3 +1,19 @@
+const he = require("he");
+const { JSDOM } = require('jsdom');
+const cheerio = require('cheerio');
+const puppeteer = require("puppeteer-extra");
+
+const campaignSelector = "#react-campaign > section > div > div"
+const videoDivSelector = "#react-project-header > div > div > div.grid-row.grid-row.mb5-lg.mb0-md.order1-md.order2-lg > div.grid-col-12.grid-col-8-lg > div.mx-4.mx-12-md.mx0-lg.relative.clip"
+const videoSelector = "#react-project-header > div > div.grid-container.flex.flex-column > div.grid-row.grid-row.mb5-lg.mb0-md.order1-md.order2-lg > div.grid-col-12.grid-col-8-lg > div.mx-4.mx-12-md.mx0-lg.relative.clip > div > video"
+const searchButtonSelector =  "#global-header > section > section.section_global-nav-right.justify-end.order3.flex.grow1-md.pr3.pr4-sm.items-center.py2.basis50p-md > button";
+const searchFieldSelector = "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.p0.flex.z-dropdown-3.relative.shadow-2.items-center > input"
+const searchResultsSelector = "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.z-dropdown-3.scroll-y.relative.webkit-scrolling.bg-white > ul"
+
+const shortSleepInterval = 5000;
+const mediumSleepInterval = 7000;
+const longSleepInterval = 15000;
+
 const sleep = (waitTimeInMs) =>
   new Promise((resolve) => setTimeout(resolve, waitTimeInMs));
 
@@ -8,15 +24,85 @@ function loadDataset() {
   return data;
 }
 
+function extractCampaignText(campaignHTML) {
+  var $ = cheerio.load(campaignHTML);
+
+  $('body *').each(function() {
+    const element = $(this);
+    if (element.text().trim().length > 0) {
+      element.after(' ');
+    }
+  });
+  
+  const allText = $.text();
+  return allText;
+}
+
 (async () => {
-  const he = require("he");
-  const { JSDOM } = require('jsdom');
-  const cheerio = require('cheerio');
-  const puppeteer = require("puppeteer-extra");
   const StealthPlugin = require("puppeteer-extra-plugin-stealth");
   puppeteer.use(StealthPlugin());
   const { executablePath } = require("puppeteer");
 
+  async function searchForProject(searchTerm) {
+    await Promise.all([
+      await page.click(
+         searchButtonSelector
+       ),
+     ]);
+
+     await page.focus(searchFieldSelector);
+     await page.keyboard.type(searchTerm);
+  }
+
+  async function parseSearchResult() {
+    const searchResults = await page.$(searchResultsSelector);
+    const resultsHTML = await page.evaluate(
+      (element) => element.innerHTML,
+      searchResults
+    );
+    decoded = he.decode(resultsHTML);
+    return decoded;
+  }
+
+  async function gotToSearchResult(result) {
+    $ = cheerio.load(result.parent().html());
+    const imgSrc = $('img').attr('src');
+
+    const linkElementHandle = await page.$(`img[src="${imgSrc}"]`);
+    await linkElementHandle.click();
+    await page.waitForNavigation();
+    await sleep(longSleepInterval);
+  }
+
+  async function analyzeProjectPage() {
+    const campaignElement = await page.$(
+      campaignSelector
+    );
+    const campaignHTML = await page.evaluate(
+      (element) => element.innerHTML,
+      campaignElement
+    );
+
+    console.log(campaignHTML);
+    text = extractCampaignText(campaignHTML);
+    console.log(text);
+  }
+
+  async function deleteTextFromSearchField() {
+    await page.keyboard.down('Control');
+    await page.keyboard.press('A');
+    await page.keyboard.up('Control');
+    await page.keyboard.press('Backspace');
+  }
+
+  async function search(title) {
+    await searchForProject(title);
+    await sleep(mediumSleepInterval);
+    searchResult = await parseSearchResult();
+    var $ = cheerio.load(searchResult);
+    const result = $(`div:contains("${title}")`);
+    return result;
+  }
   titles = loadDataset();
 
   const browser = await puppeteer.launch({
@@ -29,44 +115,15 @@ function loadDataset() {
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto("https://kickstarter.com/", { waitUntil: "load" });
-    await Promise.all([
-     await page.click(
-        "#global-header > section > section.section_global-nav-right.justify-end.order3.flex.grow1-md.pr3.pr4-sm.items-center.py2.basis50p-md > button"
-      ),
-    ]);
-    await page.focus(
-      "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.p0.flex.z-dropdown-3.relative.shadow-2.items-center > input"
-    );
-    await page.keyboard.type(titles[0]);
 
-    await sleep(3000);
-
-    const pageElement = await page.$(
-      "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.z-dropdown-3.scroll-y.relative.webkit-scrolling.bg-white > ul"
-    );
-    const innerHTML = await page.evaluate(
-      (element) => element.innerHTML,
-      pageElement
-    );
-    decoded = he.decode(innerHTML);
-
-     console.log(decoded);
-     console.log("----------")
-
-    var $ = cheerio.load(decoded);
-    const result = $(`div:contains("${titles[0]}")`);
-    $ = cheerio.load(result.parent().html());
-    const imgSrc = $('img').attr('src');
-    console.log(imgSrc);
-
-    const linkElementHandle = await page.$(`img[src="${imgSrc}"]`);
-    await linkElementHandle.click();
-    await page.waitForNavigation();
-
-
-
-    // indexed = decoded.indexOf(titles[0]);
-    // if (indexed !== -1) {
-    //   console.log(indexed);
-    // }
+  for (let title in titles) {
+    const result = await search (titles[title]);
+      if (!result.html()) {
+        await deleteTextFromSearchField();
+        continue;
+      } else {
+        await gotToSearchResult(result);
+        await analyzeProjectPage();
+      }
+  }
 })();
--- strings.cjs
+++ strings.cjs
@@ -20,16 +20,36 @@ function loadDataset() {
   titles = loadDataset();
 
   
-    htmls = '<li><h3 class="mt1 mb1 navy-600 type-14 normal ml4">Projects</h3></li><li class=""><button tabindex="0" role="link" class="w100p type-18 text-left pointer pt2 pb2 pl4 pr4 flex"><img class="mr2 self-start w20 shrink0 radius2px" src="https://ksr-ugc.imgix.net/assets/012/215/713/d4f56d166cba8ae2dba02a4b6f32f03d_original.png?ixlib=rb-4.0.2&crop=faces&w=272&h=153&fit=crop&v=1463742962&auto=format&frame=1&q=92&s=6d0708eee35d7e7a345639a9367082b4" role="presentation" data-airgap-id="172"><div class="flex flex-column"><div class="type-16 dark-grey-500 medium mb3px">The Songs of Adelaide & Abullah</div><div class="type-12 soft-black mb3px hide block-sm block-md block-lg">by Michael Golden</div><div class="type-12"><div class="soft-black inline-block mr1">Unsuccessful on October 9, 2015</div></div></div></button></li>';
-    var $ = cheerio.load(htmls);
-    const result = $(`div:contains("${titles[0]}")`);
-    $ = cheerio.load(result.parent().html());
-    const imgSrc = $('img').attr('src');
-    console.log(imgSrc);
+    // htmls = '<li><h3 class="mt1 mb1 navy-600 type-14 normal ml4">Projects</h3></li><li class=""><button tabindex="0" role="link" class="w100p type-18 text-left pointer pt2 pb2 pl4 pr4 flex"><img class="mr2 self-start w20 shrink0 radius2px" src="https://ksr-ugc.imgix.net/assets/012/215/713/d4f56d166cba8ae2dba02a4b6f32f03d_original.png?ixlib=rb-4.0.2&crop=faces&w=272&h=153&fit=crop&v=1463742962&auto=format&frame=1&q=92&s=6d0708eee35d7e7a345639a9367082b4" role="presentation" data-airgap-id="172"><div class="flex flex-column"><div class="type-16 dark-grey-500 medium mb3px">The Songs of Adelaide & Abullah</div><div class="type-12 soft-black mb3px hide block-sm block-md block-lg">by Michael Golden</div><div class="type-12"><div class="soft-black inline-block mr1">Unsuccessful on October 9, 2015</div></div></div></button></li>';
+    // var $ = cheerio.load(htmls);
+    // const result = $(`div:contains("${titles[0]}")`);
+    // $ = cheerio.load(result.parent().html());
+    // const imgSrc = $('img').attr('src');
+    // console.log(imgSrc);
 
 
     // indexed = decoded.indexOf(titles[0]);
     // if (indexed !== -1) {
     //   console.log(indexed);
     // }
-})();
+
+    // htmls = '<div class="max-w113 m-auto"><div><div><div><div id="story" class="mb3"><h3 class="normal mb3 mb7-sm mobile-hide">Story</h3></div><div class="rte__content"><div>R</div></div></div></div></div><div><div><div class="pt8"><div id="risks-and-challenges" class="mb3 mb10-sm mb3 js-risks"><div class="mobile-show py3"></div><h3 class="normal mb4">Risks and challenges</h3><p class="js-risks-text text-preline">No risk. Money will be re-paid as per schedule in the event of no sale of books. Poems are already written</p><a target="_blank" class="ksr-green-700 bold type-16" href="/help/hc/sections/115001107133">Learn about accountability on Kickstarter</a></div></div></div></div><div class="border-bottom mb4 mb9-md"><div class="mb9 type-16"><span>Questions about this project? </span><a href="/projects/753774991/the-songs-of-adelaide-and-abullah/faqs" class="bold link-blue type-16"><span>Check out the FAQ</span></a></div></div><div id="report-this-project"><a class="bttn keyboard-focusable bttn-medium bttn-secondary theme--support fill-bttn-icon hover-fill-bttn-icon" href="/login?then=%2Fprojects%2F753774991%2Fthe-songs-of-adelaide-and-abullah#report-this-project" id="report-this-project-button">Report this project to Kickstarter</a></div></div>'
+    // var $ = cheerio.load(htmls);
+
+    // $('body *').each(function() {
+    //   const element = $(this);
+    //   if (element.text().trim().length > 0) {
+    //     element.after(' ');
+    //   }
+    // });
+    
+    // // console.log($.html());
+    // const allText = $.text();
+    // console.log(allText);
+
+    titles = ["baba", "deda", "stric"];
+    for (let title in titles) {
+      console.log(titles[title])
+    }
+
+  })();
