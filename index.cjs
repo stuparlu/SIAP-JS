adb98a966131a3cc8b9899906c0e9a849d6c5160
stuparche <stupar22luka@gmail.com>
2023-03-15 13:37:31 +0100

    Adjusting json file to contain other data

--- index.cjs
+++ index.cjs
@@ -1,14 +1,18 @@
 const he = require("he");
-const { JSDOM } = require('jsdom');
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 const puppeteer = require("puppeteer-extra");
 
-const campaignSelector = "#react-campaign > section > div > div"
-const videoDivSelector = "#react-project-header > div > div > div.grid-row.grid-row.mb5-lg.mb0-md.order1-md.order2-lg > div.grid-col-12.grid-col-8-lg > div.mx-4.mx-12-md.mx0-lg.relative.clip"
-const videoSelector = "#react-project-header > div > div.grid-container.flex.flex-column > div.grid-row.grid-row.mb5-lg.mb0-md.order1-md.order2-lg > div.grid-col-12.grid-col-8-lg > div.mx-4.mx-12-md.mx0-lg.relative.clip > div > video"
-const searchButtonSelector =  "#global-header > section > section.section_global-nav-right.justify-end.order3.flex.grow1-md.pr3.pr4-sm.items-center.py2.basis50p-md > button";
-const searchFieldSelector = "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.p0.flex.z-dropdown-3.relative.shadow-2.items-center > input"
-const searchResultsSelector = "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.z-dropdown-3.scroll-y.relative.webkit-scrolling.bg-white > ul"
+const campaignSelector = "#react-campaign > section > div > div";
+const videoDivSelector =
+  "#react-project-header > div > div > div.grid-row.grid-row.mb5-lg.mb0-md.order1-md.order2-lg > div.grid-col-12.grid-col-8-lg > div.mx-4.mx-12-md.mx0-lg.relative.clip";
+const videoSelector =
+  "#react-project-header > div > div.grid-container.flex.flex-column > div.grid-row.grid-row.mb5-lg.mb0-md.order1-md.order2-lg > div.grid-col-12.grid-col-8-lg > div.mx-4.mx-12-md.mx0-lg.relative.clip > div > video";
+const searchButtonSelector =
+  "#global-header > section > section.section_global-nav-right.justify-end.order3.flex.grow1-md.pr3.pr4-sm.items-center.py2.basis50p-md > button";
+const searchFieldSelector =
+  "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.p0.flex.z-dropdown-3.relative.shadow-2.items-center > input";
+const searchResultsSelector =
+  "#global-header > section > div.animation-fade-in.animation-300.animation-easeOutQuart.w100p.t0.l0.absolute.z-guided-search-3.block > div > div > div.z-dropdown-3.scroll-y.relative.webkit-scrolling.bg-white > ul";
 
 const shortSleepInterval = 5000;
 const mediumSleepInterval = 7000;
@@ -20,27 +24,35 @@ const sleep = (waitTimeInMs) =>
 function loadDataset() {
   const fs = require("fs");
   let rawdata = fs.readFileSync("titles.json");
-  let data = JSON.parse(rawdata).data;
+  let data = JSON.parse(rawdata);
   return data;
 }
+projects = loadDataset();
 
 function extractCampaignText(campaignHTML) {
   var $ = cheerio.load(campaignHTML);
 
-  const numVideos = $('video').length;
-  const numIframes = $('iframe').length;
-  const numImages = $('img').length;
-  const mediaNumber = numVideos + numIframes + numImages;
-  
-  $('body *').each(function() {
+  $("body *").each(function () {
     const element = $(this);
     if (element.text().trim().length > 0) {
-      element.after(' ');
+      element.after(" ");
     }
   });
-  
-  const allText = $.text();
+
+  var allText = $.text();
+  allText = allText.replace(/ {2,}/g, " ");
   return allText;
+}
+
+function extractCampaignMediaNumber(campaignHTML) {
+  var $ = cheerio.load(campaignHTML);
+
+  const numVideos = $("video").length;
+  const numIframes = $("iframe").length;
+  const numImages = $("img").length;
+  const mediaNumber = numVideos + numIframes + numImages;
+
+  return mediaNumber;
 }
 
 (async () => {
@@ -49,14 +61,10 @@ function extractCampaignText(campaignHTML) {
   const { executablePath } = require("puppeteer");
 
   async function searchForProject(searchTerm) {
-    await Promise.all([
-      await page.click(
-         searchButtonSelector
-       ),
-     ]);
+    await Promise.all([await page.click(searchButtonSelector)]);
 
-     await page.focus(searchFieldSelector);
-     await page.keyboard.type(searchTerm);
+    await page.focus(searchFieldSelector);
+    await page.keyboard.type(searchTerm);
   }
 
   async function parseSearchResult() {
@@ -71,7 +79,7 @@ function extractCampaignText(campaignHTML) {
 
   async function gotToSearchResult(result) {
     $ = cheerio.load(result.parent().html());
-    const imgSrc = $('img').attr('src');
+    const imgSrc = $("img").attr("src");
 
     const linkElementHandle = await page.$(`img[src="${imgSrc}"]`);
     await linkElementHandle.click();
@@ -79,37 +87,41 @@ function extractCampaignText(campaignHTML) {
     await sleep(longSleepInterval);
   }
 
-  async function analyzeProjectPage() {
-    const campaignElement = await page.$(
-      campaignSelector
-    );
+  async function analyzeProjectPage(index) {
+    const campaignElement = await page.$(campaignSelector);
     const campaignHTML = await page.evaluate(
       (element) => element.innerHTML,
       campaignElement
     );
 
-    // console.log(campaignHTML);
     text = extractCampaignText(campaignHTML);
-    // console.log(text);
+    mediaNumber = extractCampaignMediaNumber(campaignHTML);
+
+    projects[index].processed = true;
+    // projects[index].hasHeaderVideo
+    // projects[index].videoLength
+    projects[index].descriptionMediaNumber = mediaNumber;
+    projects[index].textLength = text.length;
+    projects[index].textDescription = text;
+
+    console.log(projects[index]);
   }
 
   async function deleteTextFromSearchField() {
-    await page.keyboard.down('Control');
-    await page.keyboard.press('A');
-    await page.keyboard.up('Control');
-    await page.keyboard.press('Backspace');
+    await page.keyboard.down("Control");
+    await page.keyboard.press("A");
+    await page.keyboard.up("Control");
+    await page.keyboard.press("Backspace");
   }
 
-  async function search(title) {
-    await searchForProject(title);
+  async function search(project) {
+    await searchForProject(project.title);
     await sleep(mediumSleepInterval);
     searchResult = await parseSearchResult();
     var $ = cheerio.load(searchResult);
-    const result = $(`div:contains("${title}")`);
+    const result = $(`div:contains("${project.title}")`);
     return result;
   }
-
-  titles = loadDataset();
 
   const browser = await puppeteer.launch({
     headless: false,
@@ -122,14 +134,16 @@ function extractCampaignText(campaignHTML) {
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto("https://kickstarter.com/", { waitUntil: "load" });
 
-  for (let title in titles) {
-    const result = await search(titles[title]);
+  for (let index in projects) {
+    if (!projects[index].processed) {
+      const result = await search(projects[index]);
       if (!result.html()) {
         await deleteTextFromSearchField();
         continue;
       } else {
         await gotToSearchResult(result);
-        await analyzeProjectPage();
+        await analyzeProjectPage(index);
       }
+    }
   }
 })();
